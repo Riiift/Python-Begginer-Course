13d6862d651cc838aa76af4869818c3f6f6c4982
Rift <49046616+Riiift@users.noreply.github.com>
2019-03-28 20:59:13 -0300

    Add files via upload

---
+++ 14.py
@@ -0,0 +1,27 @@
+
+print("       pppppppppppp         ")
+print("   pp                PP     ")
+print("  pp    PP    PP       PP   ")
+print(" pp     pp    pp        PP  ")
+print("pp                       PP ")
+print("pp                        pp")
+print(" pp                     pp ")
+print("   pp               pp ") 
+print("      pp   pp   pp")
+
+
+
+nombre = input("Ingrese su nombre")
+print("mi nombre e:", nombre)
+
+
+lado1 = input("ingrese el lado 1:")
+lado1 = int( lado1 )
+lado2 = input("ingrese el lado 2:")
+lado2 = int( lado2 )
+resultado = lado1 * lado2 
+print("resultado",resultado)
+
+
+
+input("Ingrese numero",)
