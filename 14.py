779a064076aae9290b5b412b8ae9fabe2f8543ad
Rift <49046616+Riiift@users.noreply.github.com>
2019-03-28 21:50:44 -0300

    Update 14.py

--- 14.py
+++ 14.py
@@ -1,27 +1,25 @@
+EJ1
+print("******************************")
+print("*     pppppppppppppppp       *")
+print("*   pp                PP     *")
+print("*  pp    ---   ---     PP    *")
+print("* pp     O      o       PP   *")
+print("*pp                     PP   *")
+print("* pp         <          pp   *")
+print("* pp     ---------     pp    *")
+print("*   pp                pp     *") 
+print("*      pp pp pp pp pp        *") 
+print("******************************")
 
-print("       pppppppppppp         ")
-print("   pp                PP     ")
-print("  pp    PP    PP       PP   ")
-print(" pp     pp    pp        PP  ")
-print("pp                       PP ")
-print("pp                        pp")
-print(" pp                     pp ")
-print("   pp               pp ") 
-print("      pp   pp   pp")
-
-
-
+EJ2
 nombre = input("Ingrese su nombre")
-print("mi nombre e:", nombre)
+print("mi nombre es:", nombre)
 
-
-lado1 = input("ingrese el lado 1:")
-lado1 = int( lado1 )
-lado2 = input("ingrese el lado 2:")
-lado2 = int( lado2 )
-resultado = lado1 * lado2 
-print("resultado",resultado)
-
+EJ3
+lado1 = int(input("ingrese el lado 1:"))
+lado2 = int(input("ingrese el lado 2:")) 
+print("Area", lado1 * lado2 )
+print("Perimetro", 2*lado1 + lado2 )
 
 
 input("Ingrese numero",)
