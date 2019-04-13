7e6693e9b3adf99e73fa7193bc102a485b40ee7b
Rift <49046616+Riiift@users.noreply.github.com>
2019-04-13 16:58:50 -0300

    Update Practica 2.py

--- Practica 2.py
+++ Practica 2.py
@@ -1,3 +1,34 @@
+                                                                   PRACTICA N°2
+#---------------------------------------------------------------------------------------------------------------------------------------
+
+EJ1 
+
+import math
+
+def areadeltriangulo(a,b,c):
+
+    p=(a+b+c/2)
+    area=math.sqrt(p*(p-a)*(p-b)*(p-c))
+    return area
+    
+def main():
+     
+    lado1=float(input("ingrese lado 1:"))
+    lado2=float(input("ingrese lado 1:"))
+    lado3=float(input("ingrese lado 1:"))
+    
+    resultado= areadeltriangulo (lado1,lado2,lado3)
+    
+    print("El area es: {:.2f}".format(resultado))
+    
+main()
+#-----------------------------------------------------------------------------------------------------
+
+EJ2
+
+
+
+
 import random
 
 def azar():
