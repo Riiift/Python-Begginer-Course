0856e9863469fae753b98422d1946f9440ec9bc1
Rift <49046616+Riiift@users.noreply.github.com>
2019-05-03 09:00:16 -0300

    Update Practica 4.py

--- Practica 4.py
+++ Practica 4.py
@@ -3,3 +3,17 @@
 
 EJ1
 
+num = int(input("Ingrese numero entero"))
+
+if num%2 < 0 or num%2 > 0:
+    
+    num = int(input("Ingrese numero entero"))
+    
+if num%2 == 0:
+    cont+=1
+    
+    
+    if num%4 = 0:
+ 
+print("total de numero ingresados{}".format(,cont))
+
