7e40d460c2c7fc67c40c91022e90c63aecb98d2b
Rift <49046616+Riiift@users.noreply.github.com>
2019-05-03 09:21:52 -0300

    Update Practica 4.py

--- Practica 4.py
+++ Practica 4.py
@@ -3,17 +3,17 @@
 
 EJ1
 
-num = int(input("Ingrese numero entero"))
+num = int(input("Ingrese numero entero: "))
+cont=0
 
-if num%2 < 0 or num%2 > 0:
+while num%2 !=0 :
+    num = int(input("Ingrese numero entero: "))
     
-    num = int(input("Ingrese numero entero"))
-    
-if num%2 == 0:
+if num%2 == 0 :
     cont+=1
+    print("Numero par. Total de numeros ingresados {}".format(cont))
     
-    
-    if num%4 = 0:
+    if num%4 == 0:
+        print("Numero par.Tambien es multiplo de '4'. Total de numeros ingresados {}".format(cont))
  
-print("total de numero ingresados{}".format(,cont))
 
