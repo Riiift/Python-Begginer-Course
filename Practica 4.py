4f5ecec587565a3dd2245a9ef0b49245494b4815
Rift <49046616+Riiift@users.noreply.github.com>
2019-05-04 19:59:47 -0300

    Update Practica 4.py

--- Practica 4.py
+++ Practica 4.py
@@ -6,29 +6,54 @@ EJ1
 num = int(input("Ingrese numero entero: "))
 cont=0
 
-while num%2 !=0 :
+while cont < 5 :
     num = int(input("Ingrese numero entero: "))
     
-if num%2 == 0 :
-    cont+=1
-    print("Numero par. Total de numeros ingresados {}".format(cont))
-    
-    if num%4 == 0:
-        print("Numero par.Tambien es multiplo de '4'. Total de numeros ingresados {}".format(cont))
+    if num%2 == 0:
+        cont+=1
+        mensaje = ("Numero par. Total de numeros pares ingresados {}".format(cont))
+      
         
+        if num%4 == 0:
+            mensaje = ("Numero par. Tambien es multiplo de '4'. Total de numeros ingresados {}".format(cont))
+            
+        print (mensaje)
+        
+print ("\nFIN")
+    
+
 #----------------------------------------------------------------------------------------------------------------------------------
 
 EJ2 
 
-num = int(input("Ingrese numeros enteros positivos (finalice con 0)"))
+def main():
 
-while num > 0:
-    cont+=1
-    
+    primero = 0
+    print("Ingrese numero enteros positivos (finalice con 0): ", end= '')
+    num = int(input())
 
+    while num !=0:
+        while num < 0:
+            num = int(input("Error, ingrese un numero positivo"))
 
-while num == 0
-print("El mayor es {} y el menor {}".format())
+        if num != 0:            
+            if primero == 0:
+                menor = num
+                mayor = num
+                primero+=1                
+            if num < menor :
+                menor = num                    
+            if num > mayor:
+                mayor = num               
+            num=int(input())
+            
+    if primero !=0:
+        print("El mayor es {} y el menor es {} .".format(mayor, menor))
+    else:
+        print ("No se ingresaron datos")
+
+main()
+
 #----------------------------------------------------------------------------------------------------------------------------------
 
 EJ3
