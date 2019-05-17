a5f17329859e7750d008ecd99dba832891b78e7c
Rift <49046616+Riiift@users.noreply.github.com>
2019-05-17 08:55:36 -0300

    Update Practica 5.py

--- Practica 5.py
+++ Practica 5.py
@@ -1,1 +1,18 @@
-holA
+def divisor(palabra):
+    
+    while len(palabra) > 2:
+       restador=[0:len(palabra)] - [len(palabra):2]
+    
+    
+    
+    
+    
+    
+    else len(palabras) < 1:
+        print ("la funcion ha retornado palabra vacia.")
+        
+def main():
+    palabra = input("ingrese palabra: ")
+    palabra_div= divisor(palabra)
+    print("la funcion ha retornado: {} ".format(palabra_div))
+main()
